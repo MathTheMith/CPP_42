ca41f35a7d1e94ce65f6a60a308432fdfb962a64
Matheo Vachon <mvachon@z3r10p3.42lyon.fr>
2025-11-27 16:57:04 +0100

    fixed errors seen at the eval

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvachon <mvachon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 15:51:42 by mvachon           #+#    #+#             */
+/*   Updated: 2025/11/27 15:51:42 by mvachon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,37 +17,33 @@
 
 int main(int ac, char **av) 
 {
-	if (ac == 4)
-	{
-		std::string s1 = av[2];
-		std::string s2 = av[3];
-		std::string output_filename = std::string(av[1]) + ".replace";
-		std::ifstream file(av[1]);
-
-		if (!file.is_open()) {
-			std::cerr << "Error: impossible to open the file." << std::endl;
-			return 1;
-		}
-		std::string line;
-		std::string final_line;
-		std::ofstream output(output_filename.c_str());
-		if (!output.is_open())
-        	std::cerr << "Error: impossible to create the file !" << std::endl;
-		while (std::getline(file, line))
-		{
-		    std::size_t pos = 0;
-		    final_line.clear();
-		    while ((pos = line.find(s1)) != std::string::npos)
-		    {
-		        final_line += line.substr(0, pos);
-		        final_line += s2;
-		        line = line.substr(pos + s1.length());
-		    }
-		    final_line += line;
-		    output << final_line << "\n";
-		}
-		file.close();
-		return 0;
+	if (ac != 4)
+		return (0);
+	std::string s1(av[2]);
+	std::string s2(av[3]);
+	std::string output_filename = std::string(av[1]) + ".replace";
+	std::ifstream file(av[1]);
+	if (!file.is_open()) {
+		std::cout << "Error: impossible to open the file." << std::endl;
+		return 1;
 	}
-	return (1);
+	std::string line;
+	std::string final_line;
+	std::ofstream output(output_filename.c_str());
+	if (!output.is_open())
+    	std::cout << "Error: impossible to create the file !" << std::endl;
+	while (std::getline(file, line))
+	{
+	    std::size_t pos = 0;
+	    final_line.clear();
+	    while ((pos = line.find(s1)) != std::string::npos)
+	    {
+	        final_line += line.substr(0, pos);
+	        final_line += s2;
+	        line = line.substr(pos + s1.length());
+	    }
+	    final_line += line;
+	    output << final_line << "\n";
+	}
+	file.close();
 }
--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -27,22 +27,32 @@ Harl::Harl()
 
 void Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my "
+	"7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!\n" << std::endl;
+	(this->*actions[1])();
 }
 
 void Harl::info(void)
 {
-	std::cout <<  "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"<< std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout <<  "I cannot believe adding extra bacon costs more money. "
+	"You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n"<< std::endl;
+	(this->*actions[2])();
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. "
+	"I’ve been coming for years, whereas you started working here just last month.\n" << std::endl;
+	(this->*actions[3])();
 }
 
 void Harl::error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
 }
 
 void Harl::complain(std::string level)
@@ -55,24 +65,16 @@ void Harl::complain(std::string level)
 	switch (i)
 	{
 		case 0:
-			std::cout << "[ DEBUG ]" << std::endl;
 			(this->*actions[0])();
-			std::cout << std::endl;
-    __attribute__((fallthrough));
+			break;
 		case 1:
-			std::cout << "[ INFO ]" << std::endl;
 			(this->*actions[1])();
-			std::cout << std::endl;
-      __attribute__((fallthrough));
+			break;
 		case 2:
-			std::cout << "[ WARNING ]" << std::endl;
 			(this->*actions[2])();
-			std::cout << std::endl;
-      __attribute__((fallthrough));
+			break;
 		case 3:
-			std::cout << "[ ERROR ]" << std::endl;
 			(this->*actions[3])();
-			std::cout << std::endl;
 			break;
 		default:
 			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
