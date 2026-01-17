575b37ac6107fb77deff15d330d5754eee4f6568
Matheo Vachon <mvachon@z3r11p2.42lyon.fr>
2026-01-17 11:47:23 +0100

    changer make name, and added try/catch

--- CPP06/ex02/Base.cpp
+++ CPP06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: mvachon <mvachon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 11:54:39 by mvachon           #+#    #+#             */
-/*   Updated: 2026/01/14 13:40:57 by mvachon          ###   ########.fr       */
+/*   Updated: 2026/01/17 11:41:24 by mvachon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,41 @@ void Base::identify(Base *p)
 
 void Base::identify(Base &p)
 {
-    A* a_ptr = dynamic_cast<A*>(&p);
-    B* b_ptr = dynamic_cast<B*>(&p);
-    C* c_ptr = dynamic_cast<C*>(&p);
-    if (a_ptr != NULL)
+    try
+    {
+        A &a_ref = dynamic_cast<A&>(p);
+        (void)a_ref;
         std::cout << "Reference found A" << std::endl;
-    else if (b_ptr != NULL)
+        return;
+    }
+    catch(const std::exception& e)
+    {
+        // std::cout << "Not A" << std::endl;
+    }
+    
+    try
+    {
+        B &b_ref = dynamic_cast<B&>(p);
+        (void)b_ref;
         std::cout << "Reference found B" << std::endl;
-    else if (c_ptr != NULL)
+        return;
+    }
+    catch(const std::exception& e)
+    {
+        // std::cout << "Not B" << std::endl;
+    }
+    
+    try
+    {
+        C &c_ref = dynamic_cast<C&>(p);
+        (void)c_ref;
         std::cout << "Reference found C" << std::endl;
+        return;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Unknown type" << std::endl;
+    }
 }
 
 Base* Base::generate()
